329aada5cce93c48b48a3aad62cd6dddfc4011e0
bhavya1442 <bhavya1442.be22@chitkara.edu.in>
2025-03-04 12:08:55 +0530

    Project setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}', // Add your paths here (adjust based on your app structure)
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
